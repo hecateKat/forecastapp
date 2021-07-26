4b31ef759cc50cce285003f41d5bc79ca4c2a645
Kat <kat.ochmanska@gmail.com>
2021-07-26 17:17:45 +0200

    refactor Controller

--- src/main/java/com/kat/forecastapp/controller/ForecastController.java
+++ src/main/java/com/kat/forecastapp/controller/ForecastController.java
@@ -6,23 +6,23 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import java.io.DataInput;
 import java.io.IOException;
 
 @RestController
 public class ForecastController {
 
+    private static final String api_key = "G9pAprFJYPJtCR1623GUFZgbKVNysUBc";
+
     @GetMapping("/cokolwiek")
     @ResponseBody
     public String showPostCode(@RequestParam String postcode) throws IOException {
-//        return getLocation(postcode);
+        LocationModel location = getLocation(postcode);
+//        String voivodeship =
         return getLocation(postcode).getKey();
     }
 
@@ -30,25 +30,22 @@ public class ForecastController {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url("http://dataservice.accuweather.com/locations/v1/postalcodes/search?q=20-570&apikey=G9pAprFJYPJtCR1623GUFZgbKVNysUBc")
+                .url("http://dataservice.accuweather.com/locations/v1/postalcodes/search?q=" + postcode + "&apikey=" + api_key)
                 .method("GET", null)
                 .build();
         Response response = client.newCall(request).execute();
 
-//
-//        String jsonStr = response.body().string();
-////
-////        JSONArray array = new JSONArray(response.body().string());
-//
-//////
-        LocationModel location = null;
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//        okhttp3.ResponseBody responseBody = client.newCall(request).execute().body();
-//        LocationModel location = objectMapper.readValue(responseBody.string(), LocationModel.class);
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////        }
-        return location;
+        JSONArray array = new JSONArray(response.body().string());
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            LocationModel location = objectMapper.readValue(array.get(0).toString(), LocationModel.class);
+            return location;
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+        return null;
     }
+
+
 }
